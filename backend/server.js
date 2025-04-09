60f7c276066ae760c3c875b3c22ef264bc3733d4
Baltasar Berretta <balti@Baltasars-MacBook-Pro-2.local>
2025-04-09 13:32:45 -0400

    updated CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 4000;
 const rateLimit = require('express-rate-limit');
 
 app.use(cors({
-  origin: ['https://emr-1.onrender.com','http://localhost:5173', 'http://localhost:3000', 'http://localhost:4000'],
+  origin: [process.env.CORS_ORIGIN,'https://emr-1.onrender.com','http://localhost:5173', 'http://localhost:3000', 'http://localhost:4000'],
   credentials: true
 }));
 app.use(express.json());
